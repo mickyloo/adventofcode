db741348989b5ff82c0d3647d6340525bc5e1e32
Micky Loo <mickydzloo@gmail.com>
2021-12-01 08:56:28 -0500

    2021 day 1

---
+++ 2021/src/main/kotlin/day01/Day01.kt
@@ -0,0 +1,28 @@
+package day01
+
+import java.io.File
+
+
+fun main() {
+
+    // part 1
+    println("part1 ")
+    val count1 = File("src/main/kotlin/day01/input.txt")
+        .readLines()
+        .windowed(2, 1)
+        .map { it[1].toInt() > it[0].toInt() }
+        .count { it };
+    println(count1)
+
+    println("part2: ")
+    val count2 = File("src/main/kotlin/day01/input.txt")
+        .readLines()
+        .asSequence()
+        .windowed(3, 1)
+        .map { it -> it.sumOf { it.toInt() }}
+        .windowed(2, 1)
+        .map { it[1] > it[0] }
+        .count { it }
+
+    println(count2)
+}
