86f6ff0cf201f2667b93fc839852d4139920357e
Micky Loo <mickydzloo@gmail.com>
2021-12-02 08:29:12 -0500

    2021 day2

---
+++ 2021/src/main/kotlin/day02/Day02.kt
@@ -0,0 +1,50 @@
+package day02
+
+import java.io.File
+
+fun main() {
+    val commands = File("src/main/kotlin/day02/input.txt")
+        .readLines()
+        .map {
+            val (command, value) = it.split(" ")
+            Pair(command, value.toInt())
+        }
+
+    part1(commands)
+    part2(commands)
+}
+
+fun part1(commands: List<Pair<String, Int>>) {
+    var dist = 0
+    var depth = 0
+
+    commands.forEach { command ->
+        when(command.first) {
+            "forward" -> dist += command.second
+            "down" -> depth += command.second
+            "up" -> depth -= command.second
+            else -> println("parse error")
+        }
+    }
+    println(dist * depth)
+}
+
+
+fun part2(commands: List<Pair<String, Int>>) {
+    var dist = 0
+    var depth = 0
+    var aim = 0
+
+    commands.forEach { command ->
+        when(command.first) {
+            "forward" -> {
+                dist += command.second
+                depth += aim * command.second
+            }
+            "down" -> aim += command.second
+            "up" -> aim -= command.second
+            else -> println("parse error")
+        }
+    }
+    println(dist * depth)
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ My solutions for adventofcode
 - 2016 - Python
 - 2017 - Python 3 / Jupyter notebook
 - 2020 - Java 15
+- 2021 - Kotlin 1.6
