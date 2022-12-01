f05eb8bf44c30e2689f669972a6e5e231694043d
Micky Loo <mickydzloo@gmail.com>
2022-12-01 08:27:39 -0500

    day 1

--- 2022/src/main/kotlin/day01/day01.kt
+++ 2022/src/main/kotlin/day01/day01.kt
@@ -1,17 +1,31 @@
 package day01
 
+import java.io.File
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    val elapsed1 = measureTimeMillis { part1() }
-    val elapsed2 = measureTimeMillis { part2() }
+    val elves = File("src/main/kotlin/day01/input.txt")
+        .readText()
+        .trim()
+        .split("\n\n", "\r\n\r\n")
+        .map { elf -> elf.split("\n", "\r\n").map { it.toInt() } }
+
+    val elapsed1 = measureTimeMillis { part1(elves) }
+    val elapsed2 = measureTimeMillis { part2(elves) }
 
     println("Part1: $elapsed1 ms")
     println("Part2: $elapsed2 ms")
 }
 
-fun part1() {
+fun part1(elves: List<List<Int>>) {
+    println(elves.maxOfOrNull { elf -> elf.sum() })
 }
 
-fun part2() {
+fun part2(elves: List<List<Int>>) {
+    val top3 = elves
+        .map { it.sum() }
+        .sortedDescending()
+        .take(3)
+        .sum()
+    println(top3)
 }
