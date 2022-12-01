326eabbfb40adab744c5ef3b659fedffe5398315
mickyloo <mickydzloo@gmail.com>
2022-12-01 02:15:43 +0000

    start 2022

---
+++ 2022/src/main/kotlin/day01/day01.kt
@@ -0,0 +1,17 @@
+package day01
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val elapsed1 = measureTimeMillis { part1() }
+    val elapsed2 = measureTimeMillis { part2() }
+
+    println("Part1: $elapsed1 ms")
+    println("Part2: $elapsed2 ms")
+}
+
+fun part1() {
+}
+
+fun part2() {
+}
