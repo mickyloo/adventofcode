edaa5c864e0b19429b2500b55a75debcd8fdfbf0
Micky Loo <mickydzloo@gmail.com>
2022-12-03 07:15:56 -0500

    day3

---
+++ 2022/src/main/kotlin/day03/day03.kt
@@ -0,0 +1,45 @@
+package day03
+
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+
+private val VALUES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    .toCharArray()
+    .mapIndexed { index, c -> c to index + 1 }
+    .toMap()
+
+fun main() {
+    val lines = File("src/main/kotlin/day03/input.txt")
+        .readLines()
+        .map { it.toCharArray() }
+
+    val elapsed1 = measureTimeMillis { part1(lines) }
+    val elapsed2 = measureTimeMillis { part2(lines) }
+
+    println("Part1: $elapsed1 ms")
+    println("Part2: $elapsed2 ms")
+}
+
+fun part1(lines: List<CharArray>) {
+    val intersects = lines
+        .map {
+            val half = it.size / 2
+            val first = it.sliceArray(0 until half)
+            val second = it.sliceArray(half until it.size)
+
+            first.intersect(second.toSet()).first()
+        }
+        .sumOf { VALUES.getOrDefault(it, 0) }
+    println(intersects)
+}
+
+fun part2(lines: List<CharArray>) {
+    val groupIntersects = lines
+        .chunked(3) {
+            it[0].intersect(it[1].toSet()).intersect(it[2].toSet()).first()
+        }
+        .sumOf { VALUES.getOrDefault(it, 0) }
+
+    println(groupIntersects)
+}
