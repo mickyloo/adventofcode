65c6f3f9b0b2536692db1a96c6924e93e4d1a91c
Micky Loo <mickydzloo@gmail.com>
2022-12-04 08:28:31 -0500

    day4

---
+++ 2022/src/main/kotlin/day04/day04.kt
@@ -0,0 +1,36 @@
+package day04
+
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val regex = Regex("""^(\d+)\-(\d+)\,(\d+)\-(\d+)$""")
+    val lines = File("src/main/kotlin/day04/input.txt")
+        .readLines()
+        .map {
+            val matches = regex.matchEntire(it)!!
+            val (e1Start, e1End, e2Start, e2End) = matches.destructured
+            e1Start.toInt()..e1End.toInt() to e2Start.toInt()..e2End.toInt()
+        }
+
+    val elapsed1 = measureTimeMillis { part1(lines) }
+    val elapsed2 = measureTimeMillis { part2(lines) }
+
+    println("Part1: $elapsed1 ms")
+    println("Part2: $elapsed2 ms")
+}
+
+fun part1(lines: List<Pair<IntRange, IntRange>>) {
+    println(lines.count { it.first fullOverlap it.second })
+}
+
+fun part2(lines: List<Pair<IntRange, IntRange>>) {
+    println(lines.count { it.first anyOverlap it.second })
+}
+
+infix fun IntRange.fullOverlap(other: IntRange): Boolean =
+    (this.first >= other.first && this.last <= other.last) || (this.first <= other.first && this.last >= other.last)
+
+infix fun IntRange.anyOverlap(other: IntRange): Boolean =
+    !(this.last < other.first || this.first > other.last)
+
