840d7848c19b0fc48d3eab9d6bc166a07812ff0d
Micky Loo <mickydzloo@gmail.com>
2022-12-08 13:28:58 -0500

    day8

--- 2022/src/main/kotlin/day08/day08.kt
+++ 2022/src/main/kotlin/day08/day08.kt
@@ -6,7 +6,7 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val trees = File("src/main/kotlin/day08/input.txt")
         .readLines()
-        .map {row ->
+        .map { row ->
             row.chunked(1).map { it.toInt() }
         }
 
@@ -21,7 +21,7 @@ fun part2(trees: List<List<Int>>) {
     val rows = trees.size
     val cols = trees[0].size
 
-    val maxScore = sequence<Pair<Int,Int>> {
+    val maxScore = sequence<Pair<Int, Int>> {
         for (row in 1 until rows - 1) {
             for (col in 1 until cols - 1) {
                 yield(row to col)
@@ -32,7 +32,7 @@ fun part2(trees: List<List<Int>>) {
         val height = trees[row][col]
 
         var viewLeft = 0
-        for(i in col - 1 downTo 0) {
+        for (i in col - 1 downTo 0) {
             viewLeft++
             if (trees[row][i] >= height) {
                 break
@@ -40,7 +40,7 @@ fun part2(trees: List<List<Int>>) {
         }
 
         var viewUp = 0
-        for(i in row - 1 downTo 0) {
+        for (i in row - 1 downTo 0) {
             viewUp++
             if (trees[i][col] >= height) {
                 break
@@ -48,7 +48,7 @@ fun part2(trees: List<List<Int>>) {
         }
 
         var viewDown = 0
-        for(i in row + 1 until  rows) {
+        for (i in row + 1 until rows) {
             viewDown++
             if (trees[i][col] >= height) {
                 break
@@ -56,7 +56,7 @@ fun part2(trees: List<List<Int>>) {
         }
 
         var viewRight = 0
-        for(i in col + 1 until cols) {
+        for (i in col + 1 until cols) {
             viewRight++
             if (trees[row][i] >= height) {
                 break
@@ -72,19 +72,25 @@ fun part2(trees: List<List<Int>>) {
 fun part1(trees: List<List<Int>>) {
     val rows = trees.size
     val cols = trees[0].size
-    var numVisible = 2 * (rows + cols) - 4 // perimeter
+    val perimeter = 2 * (rows + cols) - 4
 
-    (1 until rows - 1).forEach { row ->
-        (1 until cols - 1).forEach { col ->
-            val left = (col - 1 downTo 0).maxOfOrNull { trees[row][it] }!! >= trees[row][col]
-            val up = (row - 1 downTo 0).maxOfOrNull { trees[it][col] }!! >= trees[row][col]
-            val down = (row + 1 until  rows).maxOfOrNull { trees[it][col] }!! >= trees[row][col]
-            val right = (col + 1 until  cols).maxOfOrNull { trees[row][it] }!! >= trees[row][col]
-
-            if (!(left && up && down && right)) {
-                numVisible++
+    val visible = sequence<Pair<Int, Int>> {
+        for (row in 1 until rows - 1) {
+            for (col in 1 until cols - 1) {
+                yield(row to col)
             }
         }
+    }.count { cell ->
+        val (row, col) = cell
+        val height = trees[row][col]
+
+        val left = (col - 1 downTo 0).maxOfOrNull { trees[row][it] }!! >= height
+        val up = (row - 1 downTo 0).maxOfOrNull { trees[it][col] }!! >= height
+        val down = (row + 1 until rows).maxOfOrNull { trees[it][col] }!! >= height
+        val right = (col + 1 until cols).maxOfOrNull { trees[row][it] }!! >= height
+
+        !(left && up && down && right)
     }
-    println(numVisible)
+
+    println(perimeter + visible)
 }
