a6342f32014a75859a8549e599b2dbdf8f08d4ce
Micky Loo <mickydzloo@gmail.com>
2022-12-09 17:16:30 -0500

    wip

--- 2022/src/main/kotlin/day09/day09.kt
+++ 2022/src/main/kotlin/day09/day09.kt
@@ -4,8 +4,16 @@ import java.io.File
 import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
-enum class Direction {
-    U, D, L, R
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(delta: Delta): Point = Point(x + delta.dx, y + delta.dy)
+}
+data class Delta(val dx: Int, val dy: Int)
+
+enum class Direction(val delta: Delta) {
+    U(Delta(0, 1)),
+    D(Delta(0, -1)),
+    L(Delta(-1, 0)),
+    R(Delta(1, 0))
 }
 
 fun main() {
@@ -23,44 +31,77 @@ fun main() {
     //println("Part2: $elapsed2 ms")
 }
 
-typealias Point = Pair<Int, Int>
 
 
-private data class State(val head: Point, val tail: Point) {
-    infix fun moveTo(dir: Direction): State {
-        val newHead = when (dir) {
-            Direction.U -> head.first to head.second + 1
-            Direction.D -> head.first to head.second - 1
-            Direction.L -> head.first - 1 to head.second
-            Direction.R -> head.first + 1 to head.second
-        }
-
-        val dx = abs(tail.first - newHead.first)
-        val dy = abs(tail.second - newHead.second)
-        val newTail = when (dir) {
-            Direction.U -> if (dy > 1) newHead.first to tail.second + 1 else tail
-            Direction.D -> if (dy > 1) newHead.first to tail.second - 1 else tail
-            Direction.L -> if (dx > 1) tail.first - 1 to newHead.second else tail
-            Direction.R -> if (dx > 1) tail.first + 1 to newHead.second else tail
-        }
-
-        return State(newHead, newTail)
-    }
-}
-
+//private data class Rope(val head: Point, val tail: Point) {
+//    infix fun moveTo(dir: Direction): Rope {
+//        val newHead = moveHead(dir)
+//        val newTail = moveTail(dir, newHead)
+//        return Rope(newHead, newTail)
+//    }
+//
+//    fun moveHead(dir: Direction): Point = head + dir.delta
+//
+//    fun moveTail(dir: Direction, newHead: Point): Point {
+//        val dx = abs(tail.x - newHead.x)
+//        val dy = abs(tail.y - newHead.y)
+//        val newTail = when (dir) {
+//            Direction.U -> if (dy > 1) newHead.x to tail.y + 1 else tail
+//            Direction.D -> if (dy > 1) newHead.x to tail.y - 1 else tail
+//            Direction.L -> if (dx > 1) tail.x - 1 to newHead.y else tail
+//            Direction.R -> if (dx > 1) tail.x + 1 to newHead.y else tail
+//        }
+//        return newTail
+//    }
+//
+//    fun moveTail(newHead: Point) : Point {
+//        val dx = tail.x - newHead.x
+//        val dy = tail.y - newHead.y
+//
+//        val newTail = when {
+//            dy == 0 && abs(dx) > 1 -> tail.x + (if (dx > 0) -1 else 1) to tail.y
+//            dx == 0 && abs(dy) > 1 -> tail.x to tail.y + (if (dy > 0) -1 else 1)
+//            dy > 1 -> { newHead.x to tail.y - 1 }
+//            dy < -1 -> { newHead.x to tail.y + 1 }
+//            dx > 1 -> { tail.x - 1 to newHead.y }
+//            dx < -1 -> { tail.x + 1 to newHead.y }
+//            else -> tail  // no move
+//        }
+//
+//        return newTail
+//    }
+//}
 
 fun part1(moves: List<Pair<Direction, Int>>) {
-    val points = mutableSetOf<Point>(0 to 0)
-    var state = State(0 to 0, 0 to 0)
+    val rope = Array(2) { _ -> Point(0,0) }
+    val tails = mutableSetOf<Point>(rope.last())
+
     for ((dir, times) in moves) {
         repeat(times) {
-            state = state moveTo dir
-            points.add(state.tail)
+            head += dir.delta
+            tail += dir.delta
+            tails.add(tail)
         }
     }
-    println(points.size)
+    println(tails.size)
 }
 
-fun part2(moves: List<Pair<Direction, Int>>) {
-    TODO("Not yet implemented")
-}
+//fun part2(moves: List<Pair<Direction, Int>>) {
+//    val tails = mutableListOf<Point>(0 to 0)
+//    val segmentedRope = Array(9) { _ -> Rope(0 to 0, 0 to 0) }
+//    for ((dir, times) in moves) {
+//        repeat(times) {
+//            for((i, rope) in segmentedRope.withIndex()) {
+//                if (i > 0) {
+//                    val newHead = segmentedRope[i-1].tail
+//                    segmentedRope[i] = Rope(newHead, rope.moveTail(newHead))
+//                } else {
+//                    segmentedRope[i] = rope moveTo dir
+//                }
+//            }
+//            tails.add(segmentedRope.last().tail)
+//        }
+//    }
+//    println(tails)
+//    println(tails.toSet().size)
+//}
