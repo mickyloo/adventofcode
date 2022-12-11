02fe1352cc6924c83ee2f75ce6a81962c3be0e2e
Micky Loo <mickydzloo@gmail.com>
2022-12-11 14:37:58 -0500

    day11

--- 2022/src/main/kotlin/day11/day11.kt
+++ 2022/src/main/kotlin/day11/day11.kt
@@ -5,68 +5,63 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
 
-    val blocks = File("src/main/kotlin/day11/input.example.txt")
-        .readText()
+    val blocks = File("src/main/kotlin/day11/input.txt").readText()
         .trimEnd()
         .split("\n\n", "\r\n\r\n")
 
-    //val elapsed1 = measureTimeMillis { part1(setUpMonkeys(blocks)) }
-    val elapsed2 = measureTimeMillis { part2(setUpMonkeys(blocks)) }
+    val elapsed1 = measureTimeMillis { part1(blocks) }
+    val elapsed2 = measureTimeMillis { part2(blocks) }
 
-    //println("Part1: $elapsed1 ms")
-    //println("Part2: $elapsed2 ms")
+    println("Part1: $elapsed1 ms")
+    println("Part2: $elapsed2 ms")
 }
 
 data class Monkey(
     val order: Int,
-    val items: MutableList<Int> = mutableListOf(),
-    val transform: (Int) -> Int,
-    val modulo: Int,
+    val items: MutableList<Long> = mutableListOf(),
+    val transform: (Long) -> Long,
+    val test: Int,
     val targets: Pair<Int, Int>
-) {
-    override fun toString(): String {
-        return "$order: $items"
-    }
-}
+)
 
 fun setUpMonkeys(blocks: List<String>): List<Monkey> {
-    val monkeys = blocks.map {block ->
+    val monkeys = blocks.map { block ->
         var lines = block.lines()
-        val number = lines[0].split(" ").last().replace(":","").toInt()
-        val items = lines[1].split(": ").last().split(", ").map { it.toInt() }
+        val number = lines[0].split(" ").last().replace(":", "").toInt()
+        val items = lines[1].split(": ").last().split(", ").map { it.toLong() }
 
         val transformParts = lines[2].split(" ")
 
         val function = if (transformParts.last() == "old") {
-            { worry: Int -> (worry * worry) }
+            { worry: Long -> (worry * worry) }
         } else {
             val op = transformParts[transformParts.size - 2]
             val opValue = transformParts.last().toInt()
-            when(op) {
-                "*" -> { worry: Int -> (worry * opValue) }
-                "+" -> { worry: Int -> (worry + opValue) }
+            when (op) {
+                "*" -> { worry: Long -> (worry * opValue) }
+                "+" -> { worry: Long -> (worry + opValue) }
                 else -> throw Exception("parsing error")
             }
         }
 
-        val modulo = lines[3].split(" ").last().toInt()
+        val test = lines[3].split(" ").last().toInt()
         val trueCase = lines[4].split(" ").last().toInt()
         val falseCase = lines[5].split(" ").last().toInt()
 
-        Monkey(number, items.toMutableList(), function, modulo, trueCase to falseCase)
+        Monkey(number, items.toMutableList(), function, test, trueCase to falseCase)
     }.toList()
 
     return monkeys
 }
 
-fun part1(monkeys: List<Monkey>) {
+fun solution(monkeys: List<Monkey>, iterations: Int, limiter: (Long) -> Long) {
     val counter = monkeys.associate { it.order to 0L }.toMutableMap()
 
-    repeat(20) {
-        for(monkey in monkeys) {
-            for(worry in monkey.items) {
-                val newWorry = monkey.transform(worry) / 3
-                val newMonkey = if (newWorry % monkey.modulo == 0) monkey.targets.first else monkey.targets.second
+    repeat(iterations) {
+        for (monkey in monkeys) {
+            for (worry in monkey.items) {
+                val newWorry = limiter(monkey.transform(worry))
+                val newMonkey = if (newWorry % monkey.test == 0L) monkey.targets.first else monkey.targets.second
                 monkeys[newMonkey].items.add(newWorry)
             }
             counter[monkey.order] = counter[monkey.order]!! + monkey.items.size
@@ -74,35 +69,20 @@ fun part1(monkeys: List<Monkey>) {
         }
     }
 
-
     val result = counter.values.sortedDescending().take(2).reduce { acc, i -> acc * i }
     println(result)
-
 }
 
-fun part2(monkeys: List<Monkey>) {
-    val counter = monkeys.associate { it.order to 0L }.toMutableMap()
-    val modulo = monkeys.map { it.modulo }.reduce { acc, i -> acc * i }
-    println(modulo)
-
-    repeat(1000) {
-        println("== iteration $it ==")
-        for(monkey in monkeys) {
-            for(worry in monkey.items) {
-                val newWorry = monkey.transform(worry.mod(modulo))
-                val newMonkey = if (newWorry % monkey.modulo == 0) monkey.targets.first else monkey.targets.second
-
-                println("m ${monkey.order}, test ${monkey.modulo}, $worry -> $newWorry, throw to $newMonkey")
-                monkeys[newMonkey].items.add(newWorry)
-            }
-            counter[monkey.order] = counter[monkey.order]!! + monkey.items.size
-            monkey.items.clear()
-        }
-        println()
+fun part1(blocks: List<String>) {
+    solution(setUpMonkeys(blocks), 20) { x ->
+        x / 3
     }
-    monkeys.forEach { println(it) }
-    println(counter)
+}
 
-    val result = counter.values.sortedDescending().take(2).reduce { acc, i -> acc * i }
-    println(result)
+fun part2(blocks: List<String>) {
+    val monkeys = setUpMonkeys(blocks)
+    val lcm = monkeys.map { it.test }.reduce { acc, i -> acc * i }.toLong()
+    solution(setUpMonkeys(blocks), 10_000) { x ->
+        x.mod(lcm)
+    }
 }
