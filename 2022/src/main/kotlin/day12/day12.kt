95483439fca426d55d765656e4e3f2e628cccb8d
Micky Loo <mickydzloo@gmail.com>
2022-12-12 16:50:11 -0500

    day12

--- 2022/src/main/kotlin/day12/day12.kt
+++ 2022/src/main/kotlin/day12/day12.kt
@@ -6,56 +6,85 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
 
-    val lines = File("src/main/kotlin/day12/input.example.txt").readLines().map { it.toCharArray() }
+    val lines = File("src/main/kotlin/day12/input.txt").readLines().map { it.toCharArray() }
     var start: Point? = null
     var end: Point? = null
-    val map = mutableMapOf<Point, Char>()
+    val heights = mutableMapOf<Point, Int>()
 
     for ((y, row) in lines.withIndex()) {
-        for((x, cell) in row.withIndex()) {
-            val p = Point(x,y)
-            when(cell) {
+        for ((x, cell) in row.withIndex()) {
+            val p = Point(x, y)
+            when (cell) {
                 'S' -> {
                     start = p
-                    map[p] = 'a'
+                    heights[p] = 0
                 }
+
                 'E' -> {
                     end = p
-                    map[p] = 'z'
+                    heights[p] = 'z' - 'a'
                 }
-                else -> map[p] = cell
+
+                else -> heights[p] = cell - 'a'
             }
         }
     }
 
-    val elapsed1 = measureTimeMillis { part1(map.toMap(), start!!, end!!) }
-    val elapsed2 = measureTimeMillis { part2() }
+    val elapsed1 = measureTimeMillis { part1(heights.toMap(), start!!, end!!) }
+    val elapsed2 = measureTimeMillis { part2(heights.toMap(), end!!) }
 
     println("Part1: $elapsed1 ms")
     println("Part2: $elapsed2 ms")
 }
 
+fun part2(heights: Map<Point, Int>, end: Point) {
+    val minDist = heights
+        .filter { it.value == 0 }
+        .map { search(heights, it.key, end) }
+        .min()
+
+    println(minDist)
+}
+
+fun part1(heights: Map<Point, Int>, start: Point, end: Point) {
+    val shortestPath = search(heights, start, end)
+    println(shortestPath)
+}
+
 
 data class Point(val x: Int, val y: Int)
 
-private fun Map<Point, Char>.neighbors(p: Point): List<Point> {
+private fun Map<Point, Int>.neighbors(p: Point): List<Point> {
     val curr = this[p]!!
     return listOf(
-        Point(p.x-1, p.y),
-        Point(p.x+1, p.y),
-        Point(p.x, p.y-1),
-        Point(p.x, p.y+1)
+        Point(p.x - 1, p.y),
+        Point(p.x + 1, p.y),
+        Point(p.x, p.y - 1),
+        Point(p.x, p.y + 1)
     ).filter {
-        val neighbor: Char? = this[it]
-        neighbor != null && neighbor >= curr && (neighbor - curr) < 2
+        val neighbor: Int? = this[it]
+        neighbor != null && curr + 1 >= neighbor
     }
 }
 
-fun part1(map: Map<Point, Char>, start: Point, end: Point) {
-    println(map.neighbors(start)
-    )
-}
 
-fun part2() {
-    TODO("Not yet implemented")
+fun search(heights: Map<Point, Int>, start: Point, end: Point): Int {
+    val visited = mutableSetOf(start)
+    val queue = mutableListOf(start to 0)
+
+    while (queue.isNotEmpty()) {
+        val (current, dist) = queue.removeLast()
+        if (current == end) {
+            return dist
+        }
+        for (neighbor in heights.neighbors(current)) {
+            if (neighbor !in visited) {
+                queue.add(neighbor to dist + 1)
+                visited.add(neighbor)
+            }
+        }
+        queue.sortBy { -it.second }
+    }
+
+    return Int.MAX_VALUE
 }
