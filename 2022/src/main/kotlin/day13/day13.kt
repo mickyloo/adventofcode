d13ebaa8f72646d30fb214188c468aee71da6f4c
Micky Loo <mickydzloo@gmail.com>
2022-12-14 20:28:10 -0500

    13 and 14

--- 2022/src/main/kotlin/day13/day13.kt
+++ 2022/src/main/kotlin/day13/day13.kt
@@ -10,7 +10,6 @@ fun main() {
         .trimEnd()
         .split("\n\n", "\r\n\r\n")
 
-
     val elapsed1 = measureTimeMillis { part1(blocks) }
     val elapsed2 = measureTimeMillis { part2(blocks) }
 
@@ -19,23 +18,40 @@ fun main() {
 }
 
 fun part1(blocks: List<String>) {
-    val first = parse(blocks[0].lines()[0])
-    val second = parse(blocks[0].lines()[1])
+    val result = blocks
+        .map { block ->
+            val (left, right) = block.lines().map { parse(it) }
+            left.compareTo(right)
+        }
+        .withIndex()
+        .sumOf {
+            if (it.value < 0) it.index + 1 else 0
+        }
+
+    println(result)
 }
 
 fun part2(blocks: List<String>) {
+    val divider2 = parse("[[2]]")
+    val divider6 = parse("[[6]]")
 
+    val signals = blocks
+        .flatMap { block ->
+            block.lines().map { parse(it) }
+        }.plus(
+            listOf(divider2, divider6)
+        ).sorted()
 
+    println((signals.indexOf(divider2) + 1) * (signals.indexOf(divider6) + 1))
 }
 
 
 fun parse(s: String): NumOrList.Nested {
-
     val parsed = NumOrList.Nested(mutableListOf())
     val stack = mutableListOf(parsed)
 
     var i = 1
-    while(i < s.length) {
+    while (i < s.length) {
         when (s[i]) {
             '[' -> {
                 val subList = NumOrList.Nested(mutableListOf())
@@ -55,7 +71,7 @@ fun parse(s: String): NumOrList.Nested {
 
             '1' -> {
                 if (s[i + 1] == '0') {
-                   stack.last().list.add(NumOrList.Single(10))
+                    stack.last().list.add(NumOrList.Single(10))
                     i++ // extra increment to consume the 0 to make 10
                 } else {
                     stack.last().list.add(NumOrList.Single(1))
@@ -68,13 +84,38 @@ fun parse(s: String): NumOrList.Nested {
     return parsed
 }
 
-sealed class NumOrList {
-    data class Single(val num: Int): NumOrList() {
+sealed class NumOrList : Comparable<NumOrList> {
+    data class Single(val num: Int) : NumOrList() {
         fun toNested(): Nested = Nested(mutableListOf(this))
+        override fun compareTo(other: NumOrList): Int {
+            return when (other) {
+                is Single -> num.compareTo(other.num)
+                is Nested -> this.toNested().compareTo(other)
+            }
+        }
+
         override fun toString(): String = num.toString()
     }
 
-    data class Nested(val list: MutableList<NumOrList>): NumOrList() {
+    data class Nested(val list: MutableList<NumOrList>) : NumOrList() {
+        override fun compareTo(other: NumOrList): Int {
+            when (other) {
+                is Single -> return this.compareTo(other.toNested())
+                is Nested -> {
+                    if (list.isEmpty() && other.list.isEmpty()) return 0
+                    val zipped = list zip other.list
+                    for ((left, right) in zipped) {
+                        val result = left.compareTo(right)
+                        if (result < 0) return -1
+                        if (result > 0) return 1
+                    }
+                    if (zipped.size == list.size) return -1
+                    if (zipped.size == other.list.size) return 1
+                    return 0
+                }
+            }
+        }
+
         override fun toString(): String = "${list}"
     }
 }
