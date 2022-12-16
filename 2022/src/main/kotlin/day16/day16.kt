13a59b2ca37e05d9294dc4cac546edeea5bfd7df
Micky Loo <mickydzloo@gmail.com>
2022-12-16 14:48:15 -0500

    day16

--- 2022/src/main/kotlin/day16/day16.kt
+++ 2022/src/main/kotlin/day16/day16.kt
@@ -4,17 +4,30 @@ import java.io.File
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    val lines = File("src/main/kotlin/day16/input.txt")
-        .readLines()
+    val re = Regex("""Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.*)""")
 
-    val elapsed1 = measureTimeMillis { part1(lines) }
-    val elapsed2 = measureTimeMillis { part2(lines) }
+    val volcano = File("src/main/kotlin/day16/input.txt")
+        .readLines().associate {
+            val matches = re.matchEntire(it)!!
+            val (name, valve, tunnels) = matches.destructured
+            name to Room(name, valve.toInt(), tunnels.split(", "))
+        }
 
+    val elapsed1 = measureTimeMillis { part1(volcano) }
+//    val elapsed2 = measureTimeMillis { part2(lines) }
+//
     println("Part1: $elapsed1 ms")
-    println("Part2: $elapsed2 ms")
+//    println("Part2: $elapsed2 ms")
 }
 
-fun part1(lines: List<String>) {
+data class Room(val name: String, val valve: Int, val tunnels: List<String>) {
+
+}
+
+fun part1(volcano: Map<String, Room>) {
+
+    val stack = mutableListOf(Triple(volcano["AA"]!!, 30, 0))
+
 }
 
 fun part2(lines: List<String>) {
