34233edb534be1d1d2c2efd582a80a248f9fb946
Micky Loo <mickydzloo@gmail.com>
2022-12-17 08:10:53 -0500

    day16

--- 2022/src/main/kotlin/day16/day16.kt
+++ 2022/src/main/kotlin/day16/day16.kt
@@ -13,26 +13,29 @@ fun main() {
             name to Room(name, valve.toInt(), tunnels.split(", "))
         }
 
-    val elapsed1 = measureTimeMillis { part1(volcano) }
-//    val elapsed2 = measureTimeMillis { part2(lines) }
-//
-    println("Part1: $elapsed1 ms")
-//    println("Part2: $elapsed2 ms")
+    //val elapsed1 = measureTimeMillis { part1(volcano) }
+    val elapsed2 = measureTimeMillis { part2(volcano) }
+
+    //println("Part1: $elapsed1 ms")
+    println("Part2: $elapsed2 ms")
 }
 
-fun calcRoutes(volcano: Map<String, Room>): Map<Pair<String, String>, Int> {
+fun calcRoutes(volcano: Map<String, Room>): Map<Pair<Room, Room>, Int> {
     val valves = volcano.values.filter { it.valve > 0 }.map { it.name }
-    val routes = mutableMapOf<Pair<String, String>, Int>()
+    val routes = mutableMapOf<Pair<Room, Room>, Int>()
 
+    val beginning = volcano["AA"]!!
     for (first in valves) {
+        val firstRoom = volcano[first]!!
         for (second in valves) {
-            val pair = first to second
+            val secondRoom = volcano[second]!!
+            val pair = firstRoom to secondRoom
             if (!routes.containsKey(pair)) {
                 routes[pair] = volcano.shortestRoute(first, second)
-                routes[second to first] = routes[pair]!!
+                routes[secondRoom to firstRoom] = routes[pair]!!
             }
         }
-        routes["AA" to first] = volcano.shortestRoute("AA", first)
+        routes[beginning to firstRoom] = volcano.shortestRoute("AA", first)
     }
 
     return routes.toMap()
@@ -59,41 +62,34 @@ private fun Map<String, Room>.shortestRoute(start: String, end: String): Int {
     return Int.MAX_VALUE
 }
 
-data class Room(val name: String, val valve: Int, val tunnels: List<String>)
+data class Room(val name: String, val valve: Int, val tunnels: List<String>) {
+    override fun toString(): String = name
+}
 
 data class Path(
-    val room: String,
+    val room: Room,
     val time: Int,
     val pressure: Int,
-    val remaining: Set<String>,
+    val remaining: Set<Room>,
 )
 
 fun part1(volcano: Map<String, Room>) {
     val routes = calcRoutes(volcano)
-    val valves = volcano.values.filter { it.valve > 0 }.map { it.name }.toSet()
+    val valves = volcano.values.filter { it.valve > 0 }.toSet()
 
     var maxPressure = 0
-    val stack = mutableListOf(Path("AA", 30, 0, valves))
-    while(stack.isNotEmpty()) {
+    val stack = mutableListOf(Path(volcano["AA"]!!, 30, 0, valves))
+    while (stack.isNotEmpty()) {
         val current = stack.removeLast()
-        val room = volcano[current.room]!!
-        var time = current.time
-
-        val newPressure = if (room.valve > 0) {
-            time--
-            current.pressure + time * room.valve
-        } else {
-            current.pressure
-        }
-
-        if (newPressure > maxPressure) {
-            maxPressure = newPressure
+        if (current.pressure > maxPressure) {
+            maxPressure = current.pressure
         }
 
         current.remaining.forEach {
             val newRemaining = current.remaining - it
-            val newTime = time - routes[room.name to it]!!
-            if (newTime > 0) {
+            val newTime = current.time - (routes[current.room to it]!! + 1)
+            if (newTime >= 0) {
+                val newPressure = current.pressure + (newTime * it.valve)
                 stack.add(Path(it, newTime, newPressure, newRemaining))
             }
         }
@@ -101,5 +97,63 @@ fun part1(volcano: Map<String, Room>) {
     println(maxPressure)
 }
 
-fun part2(lines: List<String>) {
+data class TwoPath(
+    val room: List<Room>,
+    val time: List<Int>,
+    val pressure: Int,
+    val remaining: Set<Room>,
+)
+
+fun part2(volcano: Map<String, Room>) {
+    val routes = calcRoutes(volcano)
+    val valves = volcano.values.filter { it.valve > 0 }.toSet()
+
+    var maxPressure = 0
+    val beginning = volcano["AA"]!!
+    val stack = mutableListOf(TwoPath(listOf(beginning, beginning), listOf(26, 26), 0, valves))
+    while (stack.isNotEmpty()) {
+        val current = stack.removeLast()
+        println(current)
+        if (current.pressure > maxPressure) {
+            maxPressure = current.pressure
+        }
+
+        for (me in current.remaining) {
+            for (elephant in current.remaining) {
+                if (me == elephant) continue
+
+                val meTime = current.time[0] - (routes[current.room[0] to me]!! + 1)
+                val elephantTime = current.time[1] - (routes[current.room[1] to elephant]!! + 1)
+
+                if (meTime >= 0 && elephantTime >= 0) {
+                    val newRemaining = current.remaining - me - elephant
+                    val newPressure = current.pressure + (meTime * me.valve) + (elephantTime * elephant.valve)
+                    stack.add(TwoPath(listOf(me, elephant), listOf(meTime, elephantTime), newPressure, newRemaining))
+                } else if (meTime >= 0) {
+                    val newRemaining = current.remaining - me
+                    val newPressure = current.pressure + (meTime * me.valve)
+                    stack.add(
+                        TwoPath(
+                            listOf(me, current.room[1]),
+                            listOf(meTime, current.time[1]),
+                            newPressure,
+                            newRemaining
+                        )
+                    )
+                } else if (elephantTime >= 0) {
+                    val newRemaining = current.remaining - elephant
+                    val newPressure = current.pressure + (elephantTime * elephant.valve)
+                    stack.add(
+                        TwoPath(
+                            listOf(current.room[0], elephant),
+                            listOf(current.time[0], elephantTime),
+                            newPressure,
+                            newRemaining
+                        )
+                    )
+                }
+            }
+        }
+    }
+    println(maxPressure)
 }
