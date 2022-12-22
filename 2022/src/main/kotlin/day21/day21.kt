1b4504d705f593f6ba03352e211562ce879befa9
Micky Loo <mickydzloo@gmail.com>
2022-12-21 19:57:24 -0500

    day21

--- 2022/src/main/kotlin/day21/day21.kt
+++ 2022/src/main/kotlin/day21/day21.kt
@@ -6,7 +6,7 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val lines = File("src/main/kotlin/day21/input.txt")
         .readLines()
-        .map {
+        .associate {
             val (name, expression) = it.split(": ")
             name to expression
         }
@@ -18,23 +18,55 @@ fun main() {
     println("Part2: $elapsed1 ms")
 }
 
-fun part1(lines: List<Pair<String, String>>) {
-    val monkeys = mutableMapOf<String, Lazy<Long>>()
-    lines.forEach {
-        val parts = it.second.split(" ")
-        val expression = when {
-            parts.size == 1 -> lazy { parts[0].toLong() }
-            parts[1] == "*" -> lazy { monkeys[parts[0]]!!.value * monkeys[parts[2]]!!.value }
-            parts[1] == "+" -> lazy { monkeys[parts[0]]!!.value + monkeys[parts[2]]!!.value }
-            parts[1] == "-" -> lazy { monkeys[parts[0]]!!.value - monkeys[parts[2]]!!.value }
-            parts[1] == "/" -> lazy { monkeys[parts[0]]!!.value / monkeys[parts[2]]!!.value }
-            else -> throw Exception("parse error")
-        }
-        monkeys[it.first] = expression
-    }
-    println(monkeys["root"]!!.value)
+fun part1(lines: Map<String, String>) {
+    val monkeys = makeMonkeyMap(lines)
+    println(monkeys["root"]!!.invoke())
 }
 
-fun part2(lines: List<Pair<String, String>>) {
+fun part2(lines: Map<String, String>) {
+    val monkeys = makeMonkeyMap(lines)
+    val (left, _, right) = lines["root"]!!.split(" ")
 
+    val goal = monkeys[right]!!.invoke()
+
+    var bounds = 1..10_000_000_000_000L
+    val monkey = monkeys[left]!!
+    var guess = monkeys["humn"]!!.invoke()
+
+    while(true) {
+        monkeys["humn"] = { guess }
+
+        val current = monkey.invoke()
+        if (goal == current) {
+            break
+        } else if (current < goal) {
+            bounds = bounds.first..guess
+        } else {
+            bounds = guess..bounds.last
+        }
+        guess = (bounds.first + bounds.last) / 2
+    }
+
+    while(monkey.invoke() == goal) {
+        monkeys["humn"] = { --guess }
+    }
+    println(guess+1)
+}
+
+private fun makeMonkeyMap(lines: Map<String, String>): MutableMap<String, () -> Long> {
+    val monkeys = mutableMapOf<String, () -> Long>()
+    lines.forEach {
+        val parts = it.value.split(" ")
+
+        val expression = when {
+            parts.size == 1 -> ({ parts[0].toLong() })
+            parts[1] == "*" -> ({ monkeys[parts[0]]!!.invoke() * monkeys[parts[2]]!!.invoke() })
+            parts[1] == "+" -> ({ monkeys[parts[0]]!!.invoke() + monkeys[parts[2]]!!.invoke() })
+            parts[1] == "-" -> ({ monkeys[parts[0]]!!.invoke() - monkeys[parts[2]]!!.invoke() })
+            parts[1] == "/" -> ({ monkeys[parts[0]]!!.invoke() / monkeys[parts[2]]!!.invoke() })
+            else -> throw Exception("parse error")
+        }
+        monkeys[it.key] = expression
+    }
+    return monkeys
 }
