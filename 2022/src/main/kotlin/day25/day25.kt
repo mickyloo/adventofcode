47b63135348ea5885bac18923d6e46e46db8e50c
Micky Loo <mickydzloo@gmail.com>
2022-12-25 16:27:54 -0500

    part1

--- 2022/src/main/kotlin/day25/day25.kt
+++ 2022/src/main/kotlin/day25/day25.kt
@@ -3,7 +3,6 @@ package day25
 import java.io.File
 import kotlin.math.abs
 import kotlin.math.pow
-import kotlin.math.sign
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -11,7 +10,7 @@ fun main() {
         .readLines()
         .map { line ->
             line.reversed().chunked(1).map {
-                when(it) {
+                when (it) {
                     "=" -> -2
                     "-" -> -1
                     else -> it.toInt()
@@ -21,13 +20,10 @@ fun main() {
 
     val elapsed1 = measureTimeMillis { part1(numbers) }
     println("Part1: $elapsed1 ms")
-
-//    val elapsed2 = measureTimeMillis { part2(elves) }
-//    println("Part2: $elapsed2 ms")
 }
 
-val base5 = mapOf( 0 to 0, 1 to 1, 2 to 2, 3 to -2, 4 to -1)
-val snafu = mapOf( 0 to "0", 1 to "1", 2 to "2", -2 to "=", -1 to "-")
+val base5 = mapOf(0 to 0, 1 to 1, 2 to 2, 3 to -2, 4 to -1)
+val snafu = mapOf(0 to "0", 1 to "1", 2 to "2", -2 to "=", -1 to "-")
 fun List<Int>.toSnafu() =
     this.joinToString("") {
         snafu[it]!!
@@ -46,9 +42,6 @@ fun part1(numbers: List<List<Int>>) {
         }.toMutableList()
 
     sum.add(sum.size, 0)
-    println(sum)
-    println(sum.toBase10())
-
     var carries = (0..sum.size).map { 0 }.toMutableList()
 
     for ((i, value) in sum.withIndex()) {
@@ -56,19 +49,16 @@ fun part1(numbers: List<List<Int>>) {
         if (total > 0) {
             val newDigit = base5[total.mod(5)]!!
             sum[i] = newDigit
-            carries[i+1] = abs(total / 5)
+            carries[i + 1] = abs(total / 5)
             if (newDigit < 0) {
-                carries[i+1] += 1
+                carries[i + 1] += 1
             }
         } else {
-
+            sum[i] = total % 5
+            carries[i + 1] = total / 5
         }
 
     }
 
-    println(carries)
-    println(sum)
-    println(sum.toBase10())
-    //println(sum.toSnafu())
-
+    println(sum.toSnafu())
 }
