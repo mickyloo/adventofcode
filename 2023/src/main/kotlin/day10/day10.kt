8ad3ab24307cd5b6393b34ee659ee034cfa639e7
Micky Loo <mickydzloo@gmail.com>
2023-12-16 12:56:58 -0500

    day10

--- 2023/src/main/kotlin/day10/day10.kt
+++ 2023/src/main/kotlin/day10/day10.kt
@@ -1,7 +1,9 @@
 package day10
 
 import common.Point
+import common.println
 import common.readLines
+import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -17,24 +19,25 @@ fun main() {
                     '7' -> pipes[Point(col, row)] = Type.SW
                     'F' -> pipes[Point(col, row)] = Type.SE
                     'S' -> pipes[Point(col, row)] = Type.START
-                    else -> { // no op , ignore ground }
+                    else -> pipes[Point(col, row)] = Type.EMPTY
                     }
                 }
             }
-        }
 
     val start = pipes.filter { it.value == Type.START }.keys.first()
-    val elapsed1 = measureTimeMillis { part1(start, pipes.toMap()) }
+
+    val elapsed1 = measureTimeMillis { part1(start, pipes) }
     println("Part1: $elapsed1 ms")
 
-//    val elapsed2 = measureTimeMillis { part2(pipes) }
-//    println("Part2: $elapsed2 ms")
-}
-private enum class Type {
-    VERTICAL, HORIZONTAL, NE, NW, SW, SE, START
+    val elapsed2 = measureTimeMillis { part2(start, pipes) }
+    println("Part2: $elapsed2 ms")
 }
 
-private typealias PipeMap = Map<Point, Type>
+private enum class Type {
+    VERTICAL, HORIZONTAL, NE, NW, SW, SE, START, EMPTY
+}
+
+private typealias PipeMap = MutableMap<Point, Type>
 
 private fun PipeMap.neighbors(point: Point) : Set<Point> {
     val neighbors = mutableSetOf<Point>()
@@ -79,9 +82,15 @@ private fun PipeMap.neighbors(point: Point) : Set<Point> {
     return neighbors
 }
 private fun part1(start: Point, pipes: PipeMap) {
+    findLoop(start, pipes).run {
+        println(size / 2)
+    }
+}
+
+private fun findLoop(start: Point, pipes: PipeMap): Set<Point> {
     val seen = mutableSetOf(start)
     var next = start
-    while(seen.size < pipes.size) {
+    while (seen.size < pipes.size) {
         val possibleNext = pipes.neighbors(next).filter { !seen.contains(it) }
         if (possibleNext.isEmpty()) {
             break
@@ -90,5 +99,31 @@ private fun part1(start: Point, pipes: PipeMap) {
             seen.add(next)
         }
     }
-    println(seen.size / 2)
+    return seen
+}
+
+
+private fun part2(start: Point, pipes: PipeMap) {
+    pipes[start] = Type.VERTICAL
+
+    // for horizontal ray, crossing | F 7 enters loop
+    val crossCheck = setOf(Type.VERTICAL, Type.SE, Type.SW)
+    val loop = findLoop(start, pipes).toSet()
+    val horizontalBoundaries = loop.filter {
+        crossCheck.contains(pipes[it]!!)
+    }.toSet()
+
+    // use point in polygon
+    // cast a horizontal ray from 0  to x and count how many times it cross boundaries
+    // cast a vertical ray from 0  to y and count how many times it cross boundaries
+    // inside if both horizontal and vertical cross odd number of times
+    pipes
+        .filter { !loop.contains(it.key) }
+        .map { (point, _) ->
+            val horizontalRay = (0 until point.x).map { x -> Point(x, point.y) }.toSet()
+            val horizontalCross = horizontalBoundaries.intersect(horizontalRay).size
+            val outside = horizontalCross % 2 == 0
+            if (outside) 0 else 1
+        }.sum()
+        .println()
 }
